2d9210b15d310f394d0faadc8d08b51307d2e41e
salzanopm <salzanopm@gmail.com>
2022-03-02 12:58:37 +0100

    saved tags in created

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -60,7 +60,10 @@ class PostController extends Controller
         $new_post->save();
 
         // Save tags relations
-        $new_post->tags()->sync($form_data['tags']);
+        if(isset($form_data['tags'])) {
+            $new_post->tags()->sync($form_data['tags']);
+        }
+        
 
         return redirect()->route('admin.posts.show', ['post' => $new_post->id]);
     }
